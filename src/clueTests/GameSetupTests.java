22012727321cec0adbaf35c1fbee385252fdaf79
Cliff Boyce <cboyce@mines.edu>
2016-03-21 13:29:17 -0600

    Added blank tests classes for clue players part 1

---
+++ src/clueTests/GameSetupTests.java
@@ -0,0 +1,14 @@
+package clueTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameSetupTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
